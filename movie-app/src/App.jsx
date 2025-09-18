7d3f12e371845562138f1b344e9cb0a67618cddb
Bevan Tan <btan400@gmail.com>
2025-09-18 20:17:14 +1000

    test

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -5,7 +5,10 @@ const App = () => {
     <main>
       <div className = "pattern" />
       <div className = "wrapper">
-        <h1>Find <span className = "text-gradient">Movies</span> You'll Enjoy WIthout the Hassle</h1>
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className = "text-gradient">Movies</span> You'll Enjoy WIthout the Hassle</h1>
+        </header>
       </div>
     </main>
   )
