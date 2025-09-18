c7b4f201df69862368e0614e7ee2def840e4bc75
Bevan Tan <btan400@gmail.com>
2025-09-18 17:37:58 +1000

    button for Like to Liked when clicked

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -4,21 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false); //destructure an array []
+
   return (
-    <div className="card" style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}>
+    <div className="card">
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(true)}>
+        {hasLiked ? 'Linked':'Like'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
   return (
     <div className="card-container">
       <h2>Functional Arrow Component</h2>
