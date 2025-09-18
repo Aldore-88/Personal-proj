50f6c7d9af90711d890016762a6450df196b7944
Bevan Tan <btan400@gmail.com>
2025-09-18 17:07:12 +1000

    inline styles have preference over style sheet

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -5,7 +5,14 @@ import './App.css'
 
 const Card = ({title}) => {
   return (
-    <div>
+    <div className="card" style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px',
+    }}>
       <h2>{title}</h2>
     </div>
   )
@@ -13,10 +20,10 @@ const Card = ({title}) => {
 
 const App = () => {
   return (
-    <div>
+    <div className="card-container">
       <h2>Functional Arrow Component</h2>
       
-      <Card title="Star Wars" />
+      <Card title="Star Wars" rating={5} isCool={true} actors={[{name: 'Actors'}]} />
       <Card title="Avatar" />
       <Card title="The Lion King" />
     </div>
