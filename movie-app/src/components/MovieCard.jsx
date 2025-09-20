9cdf3a2824cb663f1d0d8385bb1224819c5b9101
Bevan Tan <btan400@gmail.com>
2025-09-20 13:17:22 +1000

    moviecard -> presentational component

--- movie-app/src/components/MovieCard.jsx
+++ movie-app/src/components/MovieCard.jsx
@@ -16,9 +16,18 @@ const MovieCard = ({movie :
                         <img src = "star.svg" alt="Star Icon"/>
                         <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     </div>
+
+                    <span>•</span>
+                    <p className = "lang">{original_language}</p>
+
+                    <span>•</span>
+                    <p className = "year">
+                        {release_date ? release_date.split ('-')[0] : 'N/A'}
+                    </p>
                 </div>
 
             </div>
+
         </div>
     )
 }
