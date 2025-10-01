7ccd6d07898cc8b513816f6743a59cf6831af79a
Bevan Tan <btan400@gmail.com>
2025-10-01 17:34:14 +1000

    routes app.js

--- backend-proj-subscription-tracker/app.js
+++ backend-proj-subscription-tracker/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import {PORT} from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import { ConnectionStates } from 'mongoose';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
+
+//read up more of this
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get ('/', (req,res) => {
     res.send('Welcome to the Subscription Tracker API!');
@@ -9,6 +19,9 @@ app.get ('/', (req,res) => {
 
 app.listen(PORT, () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+    //the function is async so use await here
+    await connectToDatabase()
 })
 
 export default app;
